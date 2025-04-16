35a088d3f0bac744dd4b1eb5c84968675ba6f7ff
Marvin Naumann <1013829+nivseb@users.noreply.github.com>
2025-04-16 21:39:05 +0200

    fix(dont create trace parent header for completed trace events): (#37)

--- tests/Component/Http/TraceParentMiddlewareTest.php
+++ tests/Component/Http/TraceParentMiddlewareTest.php
@@ -41,7 +41,8 @@ test(
      * @param Closure() : AbstractChildTraceEvent $buildTraceChild
      */
     function (Closure $buildTraceChild): void {
-        $traceEvent = $buildTraceChild();
+        $traceEvent           = $buildTraceChild();
+        $traceEvent->finishAt = null;
 
         /** @var MockInterface&RepositoryContract $storeMock */
         $storeMock = Mockery::mock(RepositoryContract::class);
