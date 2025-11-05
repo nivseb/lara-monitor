cb008d1d559652c0c2e35a835cf39c418551e9c8
Marvin Naumann <1013829+nivseb@users.noreply.github.com>
2025-11-05 22:40:17 +0100

    fix: dont send dupplicated job information (#67)

--- src/Struct/Transactions/JobTransaction.php
+++ src/Struct/Transactions/JobTransaction.php
@@ -2,8 +2,6 @@
 
 namespace Nivseb\LaraMonitor\Struct\Transactions;
 
-use Illuminate\Support\Arr;
-
 class JobTransaction extends AbstractTransaction
 {
     public string $jobId         = '';
@@ -16,23 +14,5 @@ class JobTransaction extends AbstractTransaction
     public function getName(): string
     {
         return $this->jobName;
-    }
-
-    public function getCustomContext(): ?array
-    {
-        $data = parent::getCustomContext() ?? [];
-        Arr::set($data, 'job.id', $this->jobId);
-
-        return $data;
-    }
-
-    public function getLabels(): ?array
-    {
-        $data = parent::getLabels() ?? [];
-        Arr::set($data, 'laravel_job_id', $this->jobId);
-        Arr::set($data, 'laravel_job_connection', $this->jobConnection);
-        Arr::set($data, 'laravel_job_id', $this->jobId);
-
-        return array_filter($data) ?: null;
     }
 }
