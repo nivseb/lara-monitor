e26482d1d75711ec456360fbb68af2aedcf4b6ae
Marvin Naumann <1013829+nivseb@users.noreply.github.com>
2025-11-16 15:54:32 +0100

    fix: only access trace event id via accessor

--- src/Elastic/Builder/ErrorBuilder.php
+++ src/Elastic/Builder/ErrorBuilder.php
@@ -74,7 +74,7 @@ class ErrorBuilder implements ErrorBuilderContract
         }
 
         $errorData = [
-            'id'             => $error->id,
+            'id'             => $error->getId(),
             'transaction_id' => $transaction->getId(),
             'parent_id'      => $error->parentEvent->getId(),
             'trace_id'       => $error->parentEvent->getTraceId(),
